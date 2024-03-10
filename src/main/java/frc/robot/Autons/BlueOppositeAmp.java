14c851585bbe6fac80daa09a1411ed2053e0f070
Andreas petre <andpetrederp@gmail.com>
2024-03-10 14:37:14 -0400

    blue opposite amp auton.

---
+++ src/main/java/frc/robot/Autons/BlueOppositeAmp.java
@@ -0,0 +1,209 @@
+package frc.robot.Autons;
+
+
+import java.util.List;
+
+import edu.wpi.first.hal.simulation.RoboRioDataJNI;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.constraint.CentripetalAccelerationConstraint;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.RobotState;
+import frc.robot.Subsystems.Arm.ArmCommanded;
+import frc.robot.utils.trajectory.Waypoint;
+
+//red auton
+public class BlueOppositeAmp extends AutonBase {
+    // create steps based on desired function
+    enum Step {
+        start,
+        ring1,
+        shot1,
+        shootBeforeRing2,
+        ring2,
+        shot2,
+        shootBeforeRing3,
+        ring3,
+        shot3,
+        end;
+    }
+
+    public Step step = Step.start;   
+
+    public BlueOppositeAmp(RobotState robotState) {
+        super(robotState);
+
+        startPose = new Pose2d(1.45, 4.63, Rotation2d.fromDegrees(-70)); //15.15
+
+        seedPose = true;
+    }
+    
+    Pose2d ring1 = new Pose2d(8.09, 0.7, Rotation2d.fromDegrees(0));//heading 68 deg
+    Pose2d shoot1 = new Pose2d(2.4, 3.25, Rotation2d.fromDegrees(-50));//heading 57 deg
+    Pose2d ring2 = new Pose2d(8.18, 2.41, Rotation2d.fromDegrees(10));//heading 85 deg
+    Pose2d shoot2 = new Pose2d(2.4, 3.25, Rotation2d.fromDegrees(-50));
+    Pose2d ring3 = new Pose2d(2.7, 4.17, Rotation2d.fromDegrees(-10));
+    Pose2d shoot3 = new Pose2d(2.4, 4.8, Rotation2d.fromDegrees(-20));
+
+    @Override
+    public void runAuto() {
+        SmartDashboard.putString("BlueOppositeAmpEnum", step.toString());
+        
+        if(step == Step.start){
+            driving = false;
+            runIntake = false;
+            swerveBrake = true; 
+            armCommand = ArmCommanded.shotMap;
+
+            if(timer.get() < 0.5){
+                runShooter = true;
+            }
+
+            else if (timer.get() >= 0.5){
+            trajectoryConfig = new TrajectoryConfig(2, 1);
+            trajectoryGenerator.generate(trajectoryConfig,
+                List.of(Waypoint.fromHolonomicPose(startPose, Rotation2d.fromDegrees(-70)),
+                        Waypoint.fromHolonomicPose(ring1,Rotation2d.fromDegrees(0))));
+                runShooter = false;
+                timer.reset();  
+                step = Step.ring1;        
+                runIntake = true;
+            }               
+        }
+        else if(step == Step.ring1){
+            driving = true;
+            swerveBrake = false;
+            runIntake = true;
+
+            if(timer.get() > trajectoryGenerator.getDriveTrajectory().getTotalTimeSeconds()){
+                trajectoryConfig = new TrajectoryConfig(2, 1);
+                trajectoryGenerator.generate(trajectoryConfig,
+                    List.of(Waypoint.fromHolonomicPose(ring1, Rotation2d.fromDegrees(180)),
+                            Waypoint.fromHolonomicPose(shoot1,Rotation2d.fromDegrees(120))));
+                timer.reset();    
+                runIntake = false;
+                step = Step.shot1;
+            }
+        }
+        else if(step == Step.shot1){
+            driving = true;
+            if(timer.get() > trajectoryGenerator.getDriveTrajectory().getTotalTimeSeconds()){
+                driving = false;
+                timer.reset();
+                step = Step.shootBeforeRing2;
+            }  
+        }
+        else if (step == Step.shootBeforeRing2){
+            if(timer.get() < 0.5){
+                driving = false;
+                runShooter = true;
+                armCommand = ArmCommanded.shotMap;
+            }
+
+            else {
+            trajectoryConfig = new TrajectoryConfig(2, 1);
+            trajectoryGenerator.generate(trajectoryConfig,
+                List.of(Waypoint.fromHolonomicPose(shoot1, Rotation2d.fromDegrees(-70)),
+                        Waypoint.fromHolonomicPose(ring2,Rotation2d.fromDegrees(50))));
+            timer.reset();    
+            runIntake = true;
+            step = Step.ring2;
+            }
+
+        }
+        else if (step == Step.ring2){
+            driving = true;
+            runIntake = true;
+            if(timer.get() > trajectoryGenerator.getDriveTrajectory().getTotalTimeSeconds()){
+                trajectoryConfig = new TrajectoryConfig(2, 1);
+                trajectoryGenerator.generate(trajectoryConfig,
+                    List.of(Waypoint.fromHolonomicPose(ring2, Rotation2d.fromDegrees(230)),
+                            Waypoint.fromHolonomicPose(shoot2,Rotation2d.fromDegrees(90))));
+                timer.reset();    
+                runIntake = false;
+                step = Step.shot2;
+            }
+        }
+        else if (step == Step.shot2){
+            driving = true;
+            if(timer.get() > trajectoryGenerator.getDriveTrajectory().getTotalTimeSeconds()){
+                driving = false;
+                timer.reset();    
+                step = Step.shootBeforeRing3;
+            }
+        }
+        else if (step == Step.shootBeforeRing3){
+
+            if(timer.get() < 0.5){
+                driving = false;
+                runShooter = true;
+                armCommand = ArmCommanded.shotMap;
+            }
+
+            else {
+            trajectoryConfig = new TrajectoryConfig(2, 1);
+            trajectoryGenerator.generate(trajectoryConfig,
+                List.of(Waypoint.fromHolonomicPose(shoot2, Rotation2d.fromDegrees(180)),
+                        Waypoint.fromHolonomicPose(ring3,Rotation2d.fromDegrees(0))));
+            timer.reset();    
+            runIntake = true;
+            step = Step.ring3;
+            }
+
+
+        }
+        else if (step == Step.ring3){
+            driving = true;
+            runIntake = true;
+            if(timer.get() > trajectoryGenerator.getDriveTrajectory().getTotalTimeSeconds()){
+                trajectoryConfig = new TrajectoryConfig(2, 1);
+                trajectoryGenerator.generate(trajectoryConfig,
+                    List.of(Waypoint.fromHolonomicPose(ring3, Rotation2d.fromDegrees(180)),
+                            Waypoint.fromHolonomicPose(shoot3,Rotation2d.fromDegrees(90))));
+                timer.reset();
+                runIntake = false;
+                step = Step.shot3;
+            }
+        }
+        else if (step == Step.shot3){
+            driving = true;
+            if(timer.get() > trajectoryGenerator.getDriveTrajectory().getTotalTimeSeconds()){
+                driving = false;
+                timer.reset();
+                step = Step.end;
+            }
+
+        }
+        else if (step == Step.end){
+            driving = false;
+            armCommand = ArmCommanded.shotMap;
+            runShooter = true;
+        }
+        else {
+            runShooter = false;
+            driving = false;
+            runIntake = false;
+            swerveBrake = true;
+        }
+
+        SmartDashboard.putString("Step_step", step.toString());
+        if (driving) {
+            swerveBrake = false;
+            holoDriveState = trajectoryGenerator.getDriveTrajectory().sample(timer.get());
+            rotationState = trajectoryGenerator.getHolonomicRotationSequence().sample(timer.get());
+        } else {
+            swerveBrake = true;
+        }
+
+        visualizePath();
+    }
+
+    @Override
+    public void reset() {
+        super.reset();
+        swerveBrake = false;
+        step = Step.start;
+        seedPose = false;
+    }
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -50,6 +50,7 @@ public class Robot extends TimedRobot {
   private Center4NoteBlue center4NoteBlue;
   private Right4NoteBlue right4NoteBlue;
   private AndysAuton andysAuton;
+  private BlueOppositeAmp blueOppositeAmp;
 
   // creates autonSelector
   private final SendableChooser<String> autoSelector = new SendableChooser<>();
@@ -84,6 +85,7 @@ public class Robot extends TimedRobot {
     center4NoteBlue = new Center4NoteBlue(robotState);
     right4NoteBlue = new Right4NoteBlue(robotState);
     andysAuton = new AndysAuton(robotState);
+    blueOppositeAmp = new BlueOppositeAmp(robotState);
 
     newAuto = new NewAuto(robotState);
 
@@ -132,7 +134,8 @@ public class Robot extends TimedRobot {
     //   autonCommander.setAuto(center4Note);
     // }
 
-    autonCommander.setAuto(andysAuton);
+    //autonCommander.setAuto(andysAuton);
+    autonCommander.setAuto(blueOppositeAmp);
 
     drivetrain.init(autonCommander);
     shooter.reset();
