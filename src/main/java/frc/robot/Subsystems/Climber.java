d6087652233559cacbe840381c81d4fb7bb1ae03
09FALLDAMAGE <maya.j.vivio@gmail.com>
2024-04-04 16:45:41 -0400

    flip climber

--- src/main/java/frc/robot/Subsystems/Climber.java
+++ src/main/java/frc/robot/Subsystems/Climber.java
@@ -53,13 +53,13 @@ public class Climber implements SubsystemBase {
         //     climberMotor.setVoltage(0);
         // }
 
-        if ((commander.climberUp() && climberPosition.getValueAsDouble() >= -113.5) || (commander.climberUp() && commander.climberOverride()) ) {
-            climberMotor.setVoltage(-10);  
+        if ((commander.climberUp() && climberPosition.getValueAsDouble() <= 113.5) || (commander.climberUp() && commander.climberOverride()) ) {
+            climberMotor.setVoltage(10);  
           
-        } else if ((commander.climberDown() && climberPosition.getValueAsDouble() <= -70.2)) {
-            climberMotor.setVoltage(8);
-        } else if ((commander.climberDown() && climberPosition.getValueAsDouble() <= 10.7) || (commander.climberDown() && commander.climberOverride())) {
-            climberMotor.setVoltage(12);
+        } else if ((commander.climberDown() && climberPosition.getValueAsDouble() >= 70.2)) {
+            climberMotor.setVoltage(-8);
+        } else if ((commander.climberDown() && climberPosition.getValueAsDouble() >= -10.7) || (commander.climberDown() && commander.climberOverride())) {
+            climberMotor.setVoltage(-12);
         } else {
             climberMotor.setVoltage(0);
         }
