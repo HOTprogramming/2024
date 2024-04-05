a6bcd45b0afaa6b86d00399b8031fcee900f314e
ThatRudeDuck <77365903+ThatRudeDuck@users.noreply.github.com>
2024-04-05 12:54:05 -0400

    blue shotmap and auto hint

--- src/main/java/frc/robot/ConstantsFolder/CompBotConstants.java
+++ src/main/java/frc/robot/ConstantsFolder/CompBotConstants.java
@@ -293,7 +293,7 @@ public class CompBotConstants extends ConstantsBase {
             BLUEANGLE1 = 148.0;
             BLUEANGLE2 = 134.2; // 133.4
             BLUEANGLE3 = 126.0; // 125.5
-            BLUEANGLE4 = 122.3; // 122.0
+            BLUEANGLE4 = 123.5; // 122.0
             BLUEANGLE5 = 119.9;
             BLUEANGLE6 = 117.8; 
 
