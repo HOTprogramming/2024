3217508bfe9e27263d0041fae40d1796944319d9
ThatRudeDuck <77365903+ThatRudeDuck@users.noreply.github.com>
2024-04-20 10:19:47 -0400

    adjust low shotmap

--- src/main/java/frc/robot/ConstantsFolder/CompBotConstants.java
+++ src/main/java/frc/robot/ConstantsFolder/CompBotConstants.java
@@ -313,9 +313,9 @@ public class CompBotConstants extends ConstantsBase {
             REDDISTANCE5 = 8.2; // WHITE LINE
             REDDISTANCE6 = 8.3; // WHITE LINE
             REDANGLE1 = 148.5;
-            REDANGLE2 = 131.1;
-            REDANGLE3 = 126.5;
-            REDANGLE4 = 123.6;
+            REDANGLE2 = 131.2;
+            REDANGLE3 = 126.6;
+            REDANGLE4 = 123.7;
             REDANGLE5 = 122.4;  
             REDANGLE6 = 122.3;
             
